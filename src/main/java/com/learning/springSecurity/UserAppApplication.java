c7e1306b27fb46bf1da2fac8da0ee5e102b3540d
Anuj <anujs1991@gmail.com>
2021-12-14 17:53:08 +0530

    Added entityManagerFactory bean

--- src/main/java/com/learning/springSecurity/UserAppApplication.java
+++ src/main/java/com/learning/springSecurity/UserAppApplication.java
@@ -2,12 +2,19 @@ package com.learning.springSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class UserAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserAppApplication.class, args);
+    }
 
+    @Bean(name = "entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        return sessionFactory;
+    }
 }
