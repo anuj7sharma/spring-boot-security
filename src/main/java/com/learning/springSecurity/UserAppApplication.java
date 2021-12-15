48062808e57a2d0e8ddb92c0c9442624075028b9
Anuj <anujs1991@gmail.com>
2021-12-15 21:04:35 +0530

    updated application class

--- src/main/java/com/learning/springSecurity/UserAppApplication.java
+++ src/main/java/com/learning/springSecurity/UserAppApplication.java
@@ -2,8 +2,9 @@ package com.learning.springSecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class UserAppApplication {
 
     public static void main(String[] args) {
